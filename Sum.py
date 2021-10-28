68104af19e1d2641daadf79d65bc3f7a894eb162
Shalini329 <67599727+Shalini329@users.noreply.github.com>
2021-10-28 12:18:21 +0530

    Create Sum.py

---
+++ Sum.py
@@ -0,0 +1,8 @@
+in_num1 = 10 
+in_num2 = 15 
+print ("1st Input number : ", in_num1)  
+print ("2nd Input number : ", in_num2)  
+ 
+out_num = geek.add(in_num1, in_num2)  
+print ("output number after addition : ", out_num) 
+
